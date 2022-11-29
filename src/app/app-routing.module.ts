8851125eabcf0984882dd1e7da413399b96ea34d
Willian Lima <william_slayer@hotmail.com>
2022-11-29 11:09:27 -0300

    Criado componentes principais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { AlunosComponent } from './alunos/alunos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'dashboard',pathMatch:'full'},
+  {path: 'professores', component:ProfessoresComponent},
+  {path:'alunos', component: AlunosComponent},
+  {path:'perfil',component: PerfilComponent},
+  {path:'dashboard',component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
