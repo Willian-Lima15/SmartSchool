a71571587ad244dd03f2acdb52c86a65fea434a4
Willian Lima <william_slayer@hotmail.com>
2022-11-30 10:48:36 -0300

    Finalizado template de cursos

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { AlunosModel } from '../shared/alunos';
 
 @Component({
@@ -7,26 +9,47 @@ import { AlunosModel } from '../shared/alunos';
   styleUrls: ['./alunos.component.css']
 })
 export class AlunosComponent implements OnInit {
-  titulo = 'Alunos'
 
-  public alunoSelect!:AlunosModel;
+  titulo = 'Alunos'
+  alunoSelect!:AlunosModel;
+  alunosForms!: FormGroup
 
   alunos = [
     {id:1 ,nome:'Willian', sobrenome: 'Pereira', phone:54548},
     {id:2 ,nome:'Wlisses', sobrenome: 'Mota', phone:76846},
     {id:3 ,nome:'Amariles', sobrenome: 'Souza',phone:98214},
   ]
-  constructor() { }
+
+  modalRef?: BsModalRef;
+  constructor(
+    private fb: FormBuilder,
+    private modalService: BsModalService
+  ) { }
 
   ngOnInit() {
+    this.alunosForms = this.fb.group({
+      nome:['',Validators.required],
+      sobrenome:['',Validators.required],
+      phone:['',Validators.required]
+    })
   }
 
   selectAluno(aluno:AlunosModel){
-    this.alunoSelect = aluno
+    this.alunoSelect = aluno //seleciona um aluno
+    this.alunosForms.patchValue(aluno)// preenche o form com aluno selecionado
   }
 
   voltar(){
     this.alunoSelect = null as any;
+  }
+
+  salvar(){
+    console.log(this.alunosForms.value);
+
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ProfessoresModel } from '../shared/professores';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-professores',
@@ -7,26 +9,46 @@ import { ProfessoresModel } from '../shared/professores';
   styleUrls: ['./professores.component.css']
 })
 export class ProfessoresComponent implements OnInit {
-  titulo = 'Professores';
 
+  titulo = 'Professores';
   profSelect!: ProfessoresModel;
+  profForms!:FormGroup;
 
   professores = [
     {id:1 ,nome:'Tereza', disciplina: 'Português', phone:54548},
     {id:2 ,nome:'Alisson', disciplina: 'Historia', phone:76846},
     {id:3 ,nome:'Conceição', disciplina: 'Geografia',phone:98214},
   ]
-  constructor() { }
+  modalRef?: BsModalRef;
+  constructor(
+    private fb:FormBuilder,
+    private modalService: BsModalService
+  ) { }
 
   ngOnInit() {
+    this.profForms = this.fb.group({
+      nome:['',Validators.required],
+      disciplina:['',Validators.required],
+      phone:['',Validators.required]
+    })
   }
 
   selectProf(prof:ProfessoresModel) {
-    this.profSelect = prof
+    this.profSelect = prof //seleciona um professor
+    this.profForms.patchValue(prof)// preenche o form com professor selecionado
   }
 
   voltar() {
      this.profSelect =  null as any;
+  }
+
+  salvar(){
+    console.log(this.profForms.value);
+
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
